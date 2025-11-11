44741f80cb3b31ea9029d2f6b0335018bf9a2d26
yudithleon <yudithkleon@gmail.com>
2025-11-11 14:18:17 -0500

    funcionabilidad del ejercici 2

--- carritoCompras.js
+++ carritoCompras.js
@@ -1,1 +1,29 @@
+let totalProd = 0;
+let totalPre = 0;
+const producsAgreg = [];
 
+    function agregarAlCarrito(nombre, precio) {
+      totalProd++;
+      totalPre += precio;
+      producsAgreg.push(nombre);
+
+      document.getElementById("total-prod").textContent = totalProd;
+      document.getElementById("total-pre").textContent = totalPre;
+
+      console.log("Productos en el carrito:", producsAgreg);
+
+      if (totalPre > 50) {
+        alert("Envío gratis aplicado");
+      }
+    }
+
+    function vaciarCarrito() {
+      totalProd = 0;
+      totalPre = 0;
+      producsAgreg.length = 0;
+
+      document.getElementById("total-prod").textContent = totalProd;
+      document.getElementById("total-pre").textContent = totalPre;
+
+      alert("Carrito vacio");
+    }
