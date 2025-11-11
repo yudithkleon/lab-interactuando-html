95b3be9d49fc5230a484a587fdd5ae4ce22fbb0e
yudithleon <yudithkleon@gmail.com>
2025-11-11 14:09:18 -0500

    funcionabilidad del ejercicio 1 lista

--- scripts.js
+++ scripts.js
@@ -1,29 +1,40 @@
-const mostrar = document.getElementById("mostrar")
-let votosD= 0
-let votosJs=0
-let votosBD=0
+const mostrar = document.getElementById("mostrar");
+let votosD = 0;
+let votosJs = 0;
+let votosBD = 0;
 
-const diseño =()=>{
-    votosD+=1
-
-mostrar.innerHTML = `
+const diseño = () => {
+  votosD += 1;
+  mostrarConsole(votosD);
+  mostrar.innerHTML = `
 <span>Votos Diseño:${votosD}</span>
 `;
-alert("Gracias por su voto de Diseño")
-}
+  alert("Gracias por su voto de Diseño");
+  ;
+};
 
-const js =()=>{
-    votosJs+=1
-alert("Gracias por su voto de Javascripts")
-mostrar.innerHTML = `
+const js = () => {
+  votosJs += 1;
+  mostrarConsole(votosJs);
+
+  alert("Gracias por su voto de Javascripts");
+  mostrar.innerHTML = `
 <span>Votos de Js:${votosJs}</span>
 `;
-}
+};
 
-const baseDato =()=>{
-    votosBD+=1
-alert("Gracias por su voto de Base de Datos")
-mostrar.innerHTML = `
+const baseDato = () => {
+  votosBD += 1;
+  mostrarConsole(votosBD);
+
+  alert("Gracias por su voto de Base de Datos");
+  mostrar.innerHTML = `
 <span>Votos de Base de Dato:${votosBD}</span>
 `;
-}
+};
+
+const mostrarConsole = (votos) => {
+  if (votos % 5 === 0) {
+    console.log("Sus votos son: ", votos);
+  }
+};
